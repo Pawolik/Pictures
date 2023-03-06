bbd2bbe40b00d84fed01556fe76f9847e1ece475
Paweł Jacewicz <pjacewicz474@gmail.com>
2023-03-06 18:40:55 +0100

    Dodaj pliki projektów.

---
+++ Pictures/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace Pictures
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pbxBox = new System.Windows.Forms.PictureBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.Zdjęcie = new System.Windows.Forms.OpenFileDialog();
+            this.btnSave = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbxBox
+            // 
+            this.pbxBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pbxBox.Location = new System.Drawing.Point(12, 42);
+            this.pbxBox.Name = "pbxBox";
+            this.pbxBox.Size = new System.Drawing.Size(963, 550);
+            this.pbxBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbxBox.TabIndex = 0;
+            this.pbxBox.TabStop = false;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(13, 13);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(84, 23);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Dodaj zdjęcie";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(190, 12);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(81, 23);
+            this.btnClear.TabIndex = 2;
+            this.btnClear.Text = "Usuń zdjęcie";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Visible = false;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Zdjęcie
+            // 
+            this.Zdjęcie.FileName = "ofdPicture";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(103, 13);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(81, 23);
+            this.btnSave.TabIndex = 3;
+            this.btnSave.Text = "Zapisz";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1005, 604);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.pbxBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pbxBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbxBox;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.OpenFileDialog Zdjęcie;
+        private System.Windows.Forms.Button btnSave;
+    }
+}
+
---
+++ Pictures/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Pictures
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            MessageBox.Show("Program pozwala wyświelić pliki z dysku. \n Jeżeli chcesz aby program pamiętał wybrany obraz kliknij ZAPISZ. \n W celu wyczyszczenia programu kliknij usuń.");
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            Zdjęcie.ShowDialog();
+            string filePath = Zdjęcie.FileName;
+            pbxBox.Image = Image.FromFile(filePath);
+            if (filePath != null)
+            {
+                btnClear.Visible = true;
+            }
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            pbxBox.Image = null;
+            btnClear.Visible = false;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            using (SaveFileDialog saveFileDialog = new SaveFileDialog() { Filter = @"JPG|*.jpg" })
+            {
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    pbxBox.Image.Save(saveFileDialog.FileName);
+                }
+            }
+        }
+    }
+}
